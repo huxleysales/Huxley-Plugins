ca2e942797cc298905293968597737d744a51c63
Jake_7977 <82616410+jake0477@users.noreply.github.com>
2022-10-28 09:46:29 -0500

    Update ChildCheck.lua

--- Plug-ins by devs/CCM Voice, not done/ChildCheck.lua
+++ Plug-ins by devs/CCM Voice, not done/ChildCheck.lua
@@ -1,24 +1,123 @@
-
-
-local lights = script.Parent.Parent.Body.Lights.Dome.DLights
-local CanTrigger = script.CanTrigger
-local CCMS = script.Parent.CCMSounds
-local Domes = script.Parent.Parent.Body.ChildCheckButton.Click.Domes
-local CCV = script.Parent.Parent.Body.ChildCheckButton.Click.CCV
-local BusSettings = require(script.Parent.Parent.DriveSeat.BusSettings)
+wait(0.1)
+local car = script.Parent.Parent.Parent.Parent
+local prox = script.Parent.Attachment.ProximityPrompt
+local red = script.Parent.Parent.Red
+local IsHold = script.IsHold
+local lights = script.Parent.Parent.Parent.Lights.Dome.DLights
+local CanTrigger = car.DriveSeat["Child Check"].CanTrigger
+local Domes = script.Parent.Domes
+local Sounds = script.Parent.Sounds
+local CCMS = car.DriverSeat.CCMSounds
+local BusSettings = require(script.Parent.Parent.Parent.Parent.DriveSeat.BusSettings)
 local ts = game:GetService("TweenService")
 local Tinfo = TweenInfo.new(.2, Enum.EasingStyle.Linear, Enum.EasingDirection.InOut, 0, false)
-local functions = require(script.Parent.Parent.DriveSeat.BusFunctions)
+local functions = require(script.Parent.Parent.Parent.Parent.DriveSeat.BusFunctions)
 
-script.Parent.BusSetup.Values.IsOn.Changed:Connect(function()
-	if script.Parent.BusSetup.Values.IsOn.Value == false and script.Parent.Occupant ~= nil then
-		wait(1)
-		CanTrigger.Value = true
-    if CCV.value == true then
-      CCMS.CCA:Play()
-    end
+
+IsHold.Changed:Connect(function()
+	while IsHold.Value == true do
+		if Sounds.Valye == true then
+			CCMS.CCMAP:Play()
+		end
+		red.Material = "Neon"
+		wait(0.05)
+		red.Material = "SmoothPlastic"
+		wait(0.05)
+	end
+end)
+
+function EnableINC(l, s)
+	if s then
+		l.BrickColor = BrickColor.new(s)
+	end
+	local a = ts:Create(l, Tinfo, {Transparency = 0.01})
+	a:Play()
+end
+
+function DisableINC(l)
+	local a = ts:Create(l, Tinfo, {Transparency = 1})
+	a:Play()
+end
+
+function DLL()
+	for _,v in pairs(lights:GetChildren()) do -- off
+		car.Body.Lights.Dome.DomeArea.DA.Brightness = 0
+		v.Material = Enum.Material.SmoothPlastic
+	end
+	wait(0.4)
+	for _,v in pairs(lights:GetChildren()) do -- on
+		car.Body.Lights.Dome.DomeArea.DA.Brightness = 2
+		v.Material = Enum.Material.Neon
+	end
+	wait(0.4)
+	for _,v in pairs(lights:GetChildren()) do -- off
+		car.Body.Lights.Dome.DomeArea.DA.Brightness = 0
+
+		v.Material = Enum.Material.SmoothPlastic
+	end
+	wait(0.4)
+	for _,v in pairs(lights:GetChildren()) do -- on
+		car.Body.Lights.Dome.DomeArea.DA.Brightness = 2
+		v.Material = Enum.Material.Neon
+	end
+	wait(0.4)
+	for _,v in pairs(lights:GetChildren()) do -- off
+		car.Body.Lights.Dome.DomeArea.DA.Brightness = 0
+		v.Material = Enum.Material.SmoothPlastic
+	end
+end
+
+function DLI()
+	for _,v in pairs(lights:GetChildren()) do	-- off
+		car.Body.Lights.Dome.DomeArea.DA.Brightness = 0
+		DisableINC(v)
+	end
+	wait(1)
+	for _,v in pairs(lights:GetChildren()) do -- on
+		car.Body.Lights.Dome.DomeArea.DA.Brightness = 2
+		EnableINC(v)
+	end
+	wait(1)
+	for _,v in pairs(lights:GetChildren()) do -- off
+		car.Body.Lights.Dome.DomeArea.DA.Brightness = 0
+		DisableINC(v)
+	end
+	wait(1)
+	for _,v in pairs(lights:GetChildren()) do -- on
+		car.Body.Lights.Dome.DomeArea.DA.Brightness = 2
+		EnableINC(v)
+	end
+	wait(1)
+	for _,v in pairs(lights:GetChildren()) do -- off
+		car.Body.Lights.Dome.DomeArea.DA.Brightness = 0
+		DisableINC(v)
+	end
+end
+
+prox.PromptButtonHoldBegan:Connect(function()
+	IsHold.Value = true
+end)
+
+prox.PromptButtonHoldEnded:Connect(function()
+	IsHold.Value = false
+end)
+
+prox.Triggered:Connect(function()
+	if CanTrigger.Value == true then
+		car.DriveSeat.CCActivate:Stop()
+		CanTrigger.Value = false
+		if Sounds.Value == true then 
+			CCMS.CCMS:Play()
+			wait()
+			CCMS.CCMD:Play()
+		end
 		if Domes.Value == true then
-			script.Parent.BusSetup.Lighting.Dome.Value = true
+			if BusSettings.DomeLights == "INC" then
+				DLI()
+			end
+			if BusSettings.DomeLights == "LED" then
+				DLL()
+			end
 		end
 	end
 end)
